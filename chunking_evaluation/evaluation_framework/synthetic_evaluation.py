8513a757da46a0bef233c3090bdd1e31d45bcb4b
ElementQi <1033834827@qq.com>
2024-09-06 18:55:47 +0800

    fix: support qwen queries generating

--- chunking_evaluation/evaluation_framework/synthetic_evaluation.py
+++ chunking_evaluation/evaluation_framework/synthetic_evaluation.py
@@ -2,6 +2,7 @@ from typing import List
 import os
 import json
 import random
+import re
 
 from chunking_evaluation.utils import rigorous_document_search
 from .base_evaluation import BaseEvaluation
@@ -364,3 +365,242 @@ class SyntheticEvaluation(BaseEvaluation):
 
     def question_ref_filter(self):
         self.synth_questions_df = self._get_synth_questions_df()
+
+
+class QwenSyntheticEvaluation(SyntheticEvaluation):
+    def __init__(self, corpora_paths: List[str], queries_csv_path: str, chroma_db_path:str = None, openai_api_key=None):
+        super().__init__(corpora_paths=corpora_paths, queries_csv_path=queries_csv_path, chroma_db_path=chroma_db_path, openai_api_key=openai_api_key)
+        self.client = OpenAI(api_key=openai_api_key,
+                             base_url="https://one-api.s.metames.cn:38443/v1"
+                             )
+        self.model_list = ["Qwen2-7B-Instruct-GPTQ-Int8", "Qwen2-72B-Instruct-GPTQ-Int8", "Meta-Llama-3.1-70B-Instruct"]
+        self.embed_list = ["bce-embedding-base_v1", "zpoint_large_embedding_zh", "gte-Qwen2-7B-instruct"]
+        self.model = self.model_list[2]
+        self.embed = self.embed_list[2]
+    
+    def _extract_question_and_approx_references(self, corpus, document_length=4000, prev_questions=[]):
+        if len(corpus) > document_length:
+            start_index = random.randint(0, len(corpus) - document_length)
+            document = corpus[start_index : start_index + document_length]
+        else:
+            start_index = 0
+            document = corpus
+        
+        if prev_questions is not None:
+            if len(prev_questions) > 20:
+                questions_sample = random.sample(prev_questions, 20)
+                prev_questions_str = '\n'.join(questions_sample)
+            else:
+                prev_questions_str = '\n'.join(prev_questions)
+        else:
+            prev_questions_str = ""
+
+        tagged_text, tag_indexes = self._tag_text(document)
+
+        content_1 = f"""{self.question_maker_approx_system_prompt}"""
+        content_2 = f"""{self.question_maker_approx_user_prompt.replace("{document}", tagged_text).replace("{prev_questions_str}", prev_questions_str)}"""
+
+        completion = self.client.chat.completions.create(
+            model=self.model,
+            response_format={ "type": "json_object" },
+            max_tokens=600,
+            messages=[
+                {"role": "system", "content": content_1},
+                {"role": "user", "content": content_2}
+            ]
+        )
+
+        json_response = json.loads(completion.choices[0].message.content)
+        
+        try:
+            text_references = json_response['references']
+        except KeyError:
+            raise ValueError("The response does not contain a 'references' field.")
+        try:
+            question = json_response['question']
+        except KeyError:
+            raise ValueError("The response does not contain a 'question' field.")
+
+        references = []
+        for reference in text_references:
+            reference_keys = list(reference.keys())
+
+            if len(reference_keys) != 3:
+                raise ValueError(f"Each reference must have exactly 3 keys: 'content', 'start_chunk', and 'end_chunk'. Got keys: {reference_keys}")
+
+            if 'start_chunk' not in reference_keys or 'end_chunk' not in reference_keys:
+                raise ValueError("Each reference must contain 'start_chunk' and 'end_chunk' keys.")
+
+            if 'end_chunk' not in reference_keys:
+                reference_keys.remove('content')
+                reference_keys.remove('start_chunk')
+                end_chunk_key = reference_keys[0]
+                end_index = start_index + tag_indexes[reference[end_chunk_key]+1]
+            else:
+                end_index = start_index + tag_indexes[reference['end_chunk']+1]
+
+            start_index = start_index + tag_indexes[reference['start_chunk']]
+            references.append((corpus[start_index:end_index], start_index, end_index))
+        
+        return question, references
+    
+    def _extract_question_and_references(self, corpus, document_length=4000, prev_questions=[]):
+        if len(corpus) > document_length:
+            start_index = random.randint(0, len(corpus) - document_length)
+            document = corpus[start_index : start_index + document_length]
+        else:
+            document = corpus
+        
+        if prev_questions is not None:
+            if len(prev_questions) > 20:
+                questions_sample = random.sample(prev_questions, 20)
+                prev_questions_str = '\n'.join(questions_sample)
+            else:
+                prev_questions_str = '\n'.join(prev_questions)
+        else:
+            prev_questions_str = ""
+
+        completion = self.client.chat.completions.create(
+            model=self.model,
+            response_format={ "type": "json_object" },
+            max_tokens=600,
+            messages=[
+                {"role": "system", "content": self.question_maker_system_prompt},
+                {"role": "user", "content": self.question_maker_user_prompt.replace("{document}", document).replace("{prev_questions_str}", prev_questions_str)}
+            ]
+        )
+        
+        json_response = json.loads(completion.choices[0].message.content)
+        
+        try:
+            text_references = json_response['references']
+        except KeyError:
+            raise ValueError("The response does not contain a 'references' field.")
+        try:
+            question = json_response['question']
+        except KeyError:
+            raise ValueError("The response does not contain a 'question' field.")
+
+        references = []
+        for reference in text_references:
+            if not isinstance(reference, str):
+                raise ValueError(f"Expected reference to be of type str, but got {type(reference).__name__}")
+            target = rigorous_document_search(corpus, reference)
+            if target is not None:
+                reference, start_index, end_index = target
+                references.append((reference, start_index, end_index))
+            else:
+                raise ValueError(f"No match found in the document for the given reference.\nReference: {reference}")
+        
+        return question, references
+
+    def _get_sim(self, target, references):
+        response = self.client.embeddings.create(
+            input=[target]+references,
+            model=self.embed
+        )
+        nparray1 = np.array(response.data[0].embedding)
+
+        full_sim = []
+        for i in range(1, len(response.data)):
+            nparray2 = np.array(response.data[i].embedding)
+            cosine_similarity = np.dot(nparray1, nparray2) / (np.linalg.norm(nparray1) * np.linalg.norm(nparray2))
+            full_sim.append(cosine_similarity)
+    
+        return full_sim
+    
+    def _corpus_filter_duplicates(self, corpus_id, synth_questions_df, threshold):
+        corpus_questions_df = synth_questions_df[synth_questions_df['corpus_id'] == corpus_id].copy()
+
+        count_before = len(corpus_questions_df)
+
+        corpus_questions_df.drop_duplicates(subset='question', keep='first', inplace=True)
+
+        questions = corpus_questions_df['question'].tolist()
+
+        response = self.client.embeddings.create(
+            input=questions,
+            model=self.embed
+        )
+
+        embeddings_matrix = np.array([data.embedding for data in response.data])
+
+        dot_product_matrix = np.dot(embeddings_matrix, embeddings_matrix.T)
+
+        # Create a list of tuples containing the index pairs and their similarity
+        similarity_pairs = [(i, j, dot_product_matrix[i][j]) for i in range(len(dot_product_matrix)) for j in range(i+1, len(dot_product_matrix))]
+
+        # Sort the list of tuples based on the similarity in descending order
+        similarity_pairs.sort(key=lambda x: x[2], reverse=True)
+
+        similarity_scores = np.array([x[2] for x in similarity_pairs])
+
+        most_similars = (dot_product_matrix - np.eye(dot_product_matrix.shape[0])).max(axis=1)
+
+        def filter_vectors(sim_matrix, threshold):
+            n = sim_matrix.shape[0]  # Number of vectors
+            remaining = np.ones(n, dtype=bool)  # Initialize all vectors as remaining
+
+            for i in range(n):
+                if remaining[i] == 1:  # Only check for vectors that are still remaining
+                    for j in range(i+1, n):
+                        if remaining[j] == 1 and sim_matrix[i, j] > threshold:
+                            remaining[j] = 0  # Remove vector j because it's too similar to vector i
+            
+            return remaining
+
+        rows_to_keep = filter_vectors(dot_product_matrix, threshold)
+
+        corpus_questions_df = corpus_questions_df[rows_to_keep]
+
+        count_after = len(corpus_questions_df)
+
+        print(f"Corpus: {corpus_id} - Removed {count_before - count_after} .")
+
+
+        corpus_questions_df['references'] = corpus_questions_df['references'].apply(json.dumps)
+
+        full_questions_df = pd.read_csv(self.questions_csv_path)
+        full_questions_df = full_questions_df[full_questions_df['corpus_id'] != corpus_id]
+
+        full_questions_df = pd.concat([full_questions_df, corpus_questions_df], ignore_index=True)
+        # Drop the columns 'fixed', 'worst_ref_score' and 'diff_score' if they exist
+        for col in ['fixed', 'worst_ref_score', 'diff_score']:
+            if col in full_questions_df.columns:
+                full_questions_df = full_questions_df.drop(columns=col)
+
+        full_questions_df.to_csv(self.questions_csv_path, index=False)
+
+    def _generate_corpus_questions(self, corpus_id, approx=False, n=5):
+        with open(corpus_id, 'r') as file:
+            corpus = file.read()
+
+        i = 0
+        while i < n:
+            while True:
+                try:
+                    print(f"Trying Query {i}")
+                    questions_list = self.synth_questions_df[self.synth_questions_df['corpus_id'] == corpus_id]['question'].tolist()
+                    if approx:
+                        question, references = self._extract_question_and_approx_references(corpus, 4000, questions_list)
+                    else:
+                        question, references = self._extract_question_and_references(corpus, 4000, questions_list)
+                    if len(references) > 5:
+                        raise ValueError("The number of references exceeds 5.")
+                    
+                    references = [{'content': ref[0], 'start_index': ref[1], 'end_index': ref[2]} for ref in references]
+                    new_question = {
+                        'question': question,
+                        'references': json.dumps(references),
+                        'corpus_id': corpus_id
+                    }
+
+                    new_df = pd.DataFrame([new_question])
+                    self.synth_questions_df = pd.concat([self.synth_questions_df, new_df], ignore_index=True)
+                    self._save_questions_df()
+
+                    break
+                except (ValueError, json.JSONDecodeError) as e:
+                    print(f"Error occurred: {e}")
+                    continue
+            i += 1
